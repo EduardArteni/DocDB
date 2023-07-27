564b639c12580902ca2833b8215f79e1eb54a097
fedalfred <fed.tudor@gmail.com>
2023-07-27 15:26:31 +0300

    Added lombok and refactored the code to use it.

--- docdb/src/main/java/com/docdb/user/User.java
+++ docdb/src/main/java/com/docdb/user/User.java
@@ -1,8 +1,15 @@
 package com.docdb.user;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
@@ -15,48 +22,4 @@ public class User {
     @Column(name = "account_type")
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-
-    public AccountType getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(AccountType accountType) {
-        this.accountType = accountType;
-    }
 }
